3929da2c18c4f448c74f4256085cf3847866c677
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2021-12-05 22:07:48 +0700

    feat: input parsed

--- day5-1.ts
+++ day5-1.ts
@@ -8,7 +8,16 @@ const inp = `0,9 -> 5,9
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2`
+  .split("\n")
+  .map(
+    (line) =>
+      line
+        .split(" -> ")
+        .map((position) => position.split(",").map((n) => +n)) as [
+        [number, number],
+        [number, number]
+      ]
+  )
+  .filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2);
 
-inp.split('\n')
-
-console.log(inp);
+console.log(JSON.stringify(inp));
