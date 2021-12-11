c2b2fefef0b8aa7927e659f18898aad9c6ff093e
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2021-12-11 17:05:16 +0700

    feat(11): part 2

---
+++ src/day11-2.ts
@@ -0,0 +1,77 @@
+namespace day11_2 {
+  const inp: number[][] = `7313511551
+3724855867
+2374331571
+4438213437
+6511566287
+6727245532
+3736868662
+2348138263
+2417483121
+8812617112`
+    .split("\n")
+    .map((line) => line.split("").map(Number));
+
+  const neighbours: [number, number][] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const grid = inp
+    .map((line) =>
+      line.map((energy) => ({
+        energy,
+        neighbours: [] as Point[],
+      }))
+    )
+    .flatMap((line, y, arr) =>
+      line.map(
+        (point, x) => (
+          (point.neighbours = neighbours
+            .map(([dx, dy]) => arr[y + dy]?.[x + dx])
+            .filter((p): p is Point => p !== undefined)),
+          point
+        )
+      )
+    );
+
+  let res = 0;
+
+  for (let i = 0; true; ++i) {
+    let enoughEnergy = false;
+    for (const point of grid) {
+      ++point.energy > 9 && (enoughEnergy = true);
+    }
+    while (enoughEnergy) {
+      enoughEnergy = false;
+      for (const point of grid.filter(
+        (point) => point.energy > 9 && point.energy < 200
+      )) {
+        point.energy = 200;
+        for (const neighbour of point.neighbours) {
+          ++neighbour.energy > 9 && (enoughEnergy = true);
+        }
+      }
+    }
+    if (grid.length === grid.filter((point) => point.energy >= 200).length) {
+      res = i + 1;
+      break;
+    }
+    for (const point of grid.filter((point) => point.energy >= 200)) {
+      point.energy = 0;
+    }
+  }
+
+  console.log(res);
+}
+
+interface Point {
+  energy: number;
+  neighbours: Point[];
+}
