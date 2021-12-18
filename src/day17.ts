6e3721a7604a976302372c80f5f7f3a1addb935d
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2021-12-18 18:31:46 +0700

    feat(17): part 2

--- src/day17.ts
+++ src/day17.ts
@@ -7,20 +7,40 @@ namespace day17 {
   const naturalSeriesSum = (n: number) => (Math.sqrt(8 * n + 1) - 1) / 2;
   const reverseNaturalSeriesSum = (n: number) => (n * (n + 1)) / 2;
   const inBetween = (n: number, start: number, end: number) =>
-    start <= Math.max(n, end) && Math.min(n, end) <= end;
+    n <= Math.max(start, end) && Math.min(start, end) <= n;
 
   const process = (input: string): unknown => {
     const [, x1, x2, y1, y2] = Array.from(
       input.match(/target area: x=(\d+)..(\d+), y=(-*\d+)..(-*\d+)/)!
     ).map(Number) as [unknown, number, number, number, number];
-    const xSpd = Math.floor(naturalSeriesSum(Math.max(x1, x2)));
-    const xTarget = reverseNaturalSeriesSum(xSpd);
-    if (!inBetween(xTarget, x1, x2)) throw new Error("failed 1");
-    const bottomY = Math.min(y1, y2);
-    const ySpd = Math.abs(bottomY!) - 1;
-    if (ySpd < xSpd) throw new Error("failed 2");
-    const heighestY = reverseNaturalSeriesSum(ySpd);
-    return { heighestY };
+    const [left, right] = [x1, x2].sort((a, b) => a - b) as [number, number];
+    const [bottom, top] = [y1, y2].sort((a, b) => a - b) as [number, number];
+    let count = (right - left + 1) * (Math.abs(bottom) - Math.abs(top) + 1);
+    let heighest = 0;
+    for (let xSpd = Math.ceil(naturalSeriesSum(left)); xSpd <= right; ++xSpd) {
+      for (
+        let ySpd = Math.abs(bottom) - 1;
+        ySpd > (xSpd < left ? bottom : top);
+        --ySpd
+      ) {
+        for (
+          let step = 0, x = 0, xStepSpd = xSpd, y = 0, yStepSpd = ySpd;
+          x <= right && y >= bottom;
+          ++step,
+            x += xStepSpd,
+            xStepSpd = xStepSpd > 0 ? xStepSpd - 1 : 0,
+            y += yStepSpd,
+            --yStepSpd
+        ) {
+          if (inBetween(x, left, right) && inBetween(y, bottom, top)) {
+            heighest = Math.max(heighest, reverseNaturalSeriesSum(ySpd));
+            ++count;
+            break;
+          }
+        }
+      }
+    }
+    return { heighest, count };
   };
 
   console.table(
