cf42135792030cff847963ec543bd6719a968339
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2021-12-20 15:06:24 +0700

    refactor(20)

--- src/day20.ts
+++ src/day20.ts
@@ -111,55 +111,39 @@ namespace day20 {
 .....#..#####.#.###..###..#.##....##.#..##.#.#.##...###....##.####.##..####....#...#..#.######..###.`,
   };
 
-  const enhance = (img: number[][], enhancer: number[], fill: number) => {
-    const enhanced = img
-      .slice()
-      .map((row) => row.slice())
-      .map((row) => (row.unshift(0), row.push(0), row));
-    enhanced.unshift(
-      Array.from<number>({ length: enhanced[0]?.length ?? 0 }).fill(fill)
-    );
-    enhanced.push(
-      Array.from<number>({ length: enhanced[0]?.length ?? 0 }).fill(fill)
-    );
-    return enhanced.map((row, y) =>
-      row.map(
+  const enhance = (img: string[][], enhancer: string[], fill: string) =>
+    Array.from({ length: img.length + 2 }).map((_, y) =>
+      Array.from({ length: img[0]!.length + 2 }).map(
         (_, x) =>
           enhancer[
             parseInt(
-              [
-                img[y - 2]?.[x - 2] ?? `${fill}`,
-                img[y - 2]?.[x - 1] ?? `${fill}`,
-                img[y - 2]?.[x] ?? `${fill}`,
-                img[y - 1]?.[x - 2] ?? `${fill}`,
-                img[y - 1]?.[x - 1] ?? `${fill}`,
-                img[y - 1]?.[x] ?? `${fill}`,
-                img[y]?.[x - 2] ?? `${fill}`,
-                img[y]?.[x - 1] ?? `${fill}`,
-                img[y]?.[x] ?? `${fill}`,
-              ].join(""),
+              [2, 1, 0]
+                .flatMap((yi) =>
+                  [2, 1, 0].map((xi) => img[y - yi]?.[x - xi] ?? fill)
+                )
+                .join(""),
               2
             )
           ]!
       )
     );
-  };
+
+  const symbolToNum = (c: string) => (c === "#" ? "1" : "0");
+  const lits = (img: string[][]) => img.flat().filter((p) => p === "1").length;
 
   const process = (input: string): object => {
-    const [enhancerRaw, imgRaw] = input.split("\n\n");
-    const enhancer = Array.prototype.map.call(enhancerRaw, (c) =>
-      c === "#" ? 1 : 0
-    ) as number[];
+    const [enhcRaw, imgRaw] = input.split("\n\n");
+    const enhancer = Array.prototype.map.call(enhcRaw, symbolToNum) as string[];
     const img = imgRaw!
       .split("\n")
-      .map((row) => row.split("").map((c) => (c === "#" ? 1 : 0)));
-    let imgOutput: number[][] = img;
+      .map((row) => row.split("").map(symbolToNum));
+    let imgOutput: string[][] = img;
     for (let i = 0; i < 2; ++i)
-      imgOutput = enhance(imgOutput, enhancer, i % 2 === 1 ? enhancer[0]! : 0);
-    const litCount2 = imgOutput.flat().filter((pixel) => pixel === 1).length;
+      imgOutput = enhance(imgOutput, enhancer, i % 2 ? enhancer[0]! : "0");
+    const litCount2 = lits(imgOutput);
     for (let i = 2; i < 50; ++i)
-      imgOutput = enhance(imgOutput, enhancer, i % 2 === 1 ? enhancer[0]! : 0);
-    const litCount50 = imgOutput.flat().filter((pixel) => pixel === 1).length;
+      imgOutput = enhance(imgOutput, enhancer, i % 2 ? enhancer[0]! : "0");
+    const litCount50 = lits(imgOutput);
     return { litCount2, litCount50 };
   };
 
